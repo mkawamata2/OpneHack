9ba26639749962555737fbb3fc2b790002eea456
Masatsuna Kawamata <masatsuna.kawamata@unisys.co.jp>
2018-10-23 16:34:24 +0900

    接続文字列を取得するクラスを追加

---
+++ OpenHack/OpneHackFunc2/ConnectionString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpneHackFunc2
+{
+    public class ConnectionString
+    {
+        public string Value
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("ConnectionString");
+            }
+        }
+    }
+}
