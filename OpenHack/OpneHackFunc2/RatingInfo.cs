e23747bcd895b10ad894180f77e71f560fd51cd8
Masatsuna Kawamata <masatsuna.kawamata@unisys.co.jp>
2018-10-23 16:49:11 +0900

    RatingInfo追加

---
+++ OpenHack/OpneHackFunc2/RatingInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpneHackFunc2
+{
+    public class RatingInfo
+    {
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public string LocationName { get; set; }
+        public int Rating { get; set; }
+        public string UserNotes { get; set; }
+    }
+}
