46ca7744a35603047465f78b6acb1904a2720045
Masatsuna Kawamata <masatsuna.kawamata@unisys.co.jp>
2018-10-23 17:13:00 +0900

    RatingInfoを変更

--- OpenHack/OpneHackFunc2/CreateRating.cs
+++ OpenHack/OpneHackFunc2/CreateRating.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 using System;
 using System.IO;
@@ -14,22 +15,54 @@ namespace OpneHackFunc2
     {
         [FunctionName("CreateRating")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            CreateRatingIn requestBody,
             ILogger log)
         {
-            log.Log(LogLevel.Information, ConnectionString.Value);
+            MongoClient client = new MongoClient(ConnectionString.Value);
 
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            // DB名を指定して、DBを取得
+            IMongoDatabase db = client.GetDatabase("Rating");
 
-            string name = req.Query["name"];
+            // ドキュメントの作成
+            var ratingInfo = new RatingInfo
+            {
+                Id = Guid.NewGuid(),
+                UserId = new Guid(requestBody.UserId),
+                ProductId = new Guid(requestBody.ProductId),
+                LocationName = requestBody.LocationName,
+                Timestamp = DateTimeOffset.UtcNow,
+                Rating = requestBody.Rating,
+                UserNotes = requestBody.UserNotes
+            };
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            // コレクションを指定(この場合、articlesコレクション)
+            var collection = db.GetCollection<RatingInfo>("id");
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            // Insert
+            collection.InsertOne(ratingInfo);
+
+            return new OkObjectResult(ratingInfo);
+            //string name = req.Query["name"];
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            //return name != null
+            //    ? (ActionResult)new OkObjectResult($"Hello, {name}")
+            //    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
+
+
+    public class CreateRatingIn
+    {
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+        public string LocationName { get; set; }
+        public int Rating { get; set; }
+        public string UserNotes { get; set; }
+    }
+
 }
--- OpenHack/OpneHackFunc2/RatingInfo.cs
+++ OpenHack/OpneHackFunc2/RatingInfo.cs
@@ -6,8 +6,10 @@ namespace OpneHackFunc2
 {
     public class RatingInfo
     {
+        public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public Guid ProductId { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
         public string LocationName { get; set; }
         public int Rating { get; set; }
         public string UserNotes { get; set; }
